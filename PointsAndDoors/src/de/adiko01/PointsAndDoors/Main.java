8c77f36960009a44347dcf103efba232c2110c23
adiko01 <dev@adiko01.de>
2023-04-20 19:31:20 +0200

    Main Klasse erstellt

---
+++ PointsAndDoors/src/de/adiko01/PointsAndDoors/Main.java
@@ -0,0 +1,19 @@
+package de.adiko01.PointsAndDoors;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length == 0) {
+			System.out.println("Kein Parameter");
+			PointsAndDoorsGUI.main(null);
+		} else if (args[0].equalsIgnoreCase("help")) {
+			System.out.print(Konstanten.TITLE + "\n"
+							+ "HELP \n"
+							+ "");
+		} else if (args[0].equalsIgnoreCase("gui")) {		
+			PointsAndDoorsGUI.main(null);	
+		}
+	}
+
+}
